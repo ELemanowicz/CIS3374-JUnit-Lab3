63c2db392da17a066454932e73250c3a3402f33c
tue43961 <tue43961@CIS-SE35910.tu.temple.edu>
2015-03-25 10:47:56 -0400

    d

--- Lab3/src/test/java/AdmiteratorTest/admiteratorTest.java
+++ Lab3/src/test/java/AdmiteratorTest/admiteratorTest.java
@@ -1,15 +1,18 @@
 package AdmiteratorTest;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+
 import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 @RunWith(JUnitParamsRunner.class)
 public class admiteratorTest {
 
 	@Test
-	@Parameters(method = Admiterator.admit(1,))
+	@Parameters(method = admitTest())
 	public void admitTest() {
 		fail("Not yet implemented");
 	}
